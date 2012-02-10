d8a792b0f69f9d03a6ca5c45e0d1cf08a8cf3bed
Tetsuya Ohta <ohta@raw-hide.co.jp>
2012-02-11 03:24:13 +0900

    scoped with page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,10 +1,11 @@
 class Page < ActiveRecord::Base
   belongs_to :slide
+  scope :by_slide, lambda{|page| where(slide_id: page.slide_id) }
 
   validates :body, presence: true
 
   def next_page
-    self.class.where([" id > ? ", self.id]).first
+    self.class.by_slide(self).where([" id > ? ", self.id]).order("id ASC").first
   end
 
   def current_page
@@ -12,7 +13,7 @@ class Page < ActiveRecord::Base
   end
 
   def prev_page
-    self.class.where([" id < ? ", self.id]).first
+    self.class.by_slide(self).where([" id < ? ", self.id]).order("id DESC").first
   end
 
   def have_next?
