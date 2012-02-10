040d68db14bce85a2b1809fb5a068948bd04c70f
Tetsuya Ohta <ohta@raw-hide.co.jp>
2012-02-11 05:46:39 +0900

    feedback

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  layout :set_layout
+
+  private
+  def set_layout
+    if request.headers['X-PJAX']
+      false
+    else
+      "application"
+    end
+  end
+
+  def pjax?
+    raise request.headers["X-PJAX"].present?.inspect
+  end
 end
